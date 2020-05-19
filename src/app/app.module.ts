4c6db2e6ea51a832404eed058cb696ef32a945b7
prajojo <prajwal.agar11@gmail.com>
2020-05-19 09:44:56 +0530

    SIgnUp Process + Firebase Intializatioon complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,20 @@ import { MenuComponent } from './menu/menu.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { PersonComponent } from './person/person.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
+import * as firebase from 'firebase/app';
+import  'firebase/auth';
 
+const firebaseConfig = {
+  apiKey: 'AIzaSyBhhg5GCyxgclRrl07jy_VppbXuEqwoCxc',
+  authDomain: 'scribe-4ad9f.firebaseapp.com',
+  databaseURL: 'https://scribe-4ad9f.firebaseio.com',
+  projectId: 'scribe-4ad9f',
+  storageBucket: 'scribe-4ad9f.appspot.com',
+  messagingSenderId: '554837345460',
+  appId: '1:554837345460:web:196ecfef25df8381bb43e1',
+  measurementId: 'G-QC5F0XYF0K'
+};
+firebase.initializeApp(firebaseConfig);
 
 @NgModule({
   declarations: [
--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup, FormControl, Validators} from '@angular/forms';
+import {group} from '@angular/animations';
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+import {errorObject} from 'rxjs/internal-compatibility';
+
 
 @Component({
   selector: 'app-sign-up',
@@ -9,21 +14,62 @@ import {FormBuilder, FormGroup, FormControl, Validators} from '@angular/forms';
 export class SignUpComponent implements OnInit {
 
   myForm: FormGroup;
+  message = '';
+  userError: any;
 
   constructor(public  fb: FormBuilder) {
     this.myForm = this.fb.group({
       firstName: ['', [Validators.required]],
       lastName: ['', [Validators.required]],
-      email : ['', [Validators.required]],
+      email: ['', [Validators.required]],
       password: ['', [Validators.required, Validators.minLength(8)]],
       confirmPassword: ['', [Validators.required]]
+    }, {
+      validator: this.checkIfMatchingPasswords('password', 'confirmPassword')
     });
+  }
+
+  checkIfMatchingPasswords(passwordKey: string, confirmPasswordKey: string) {
+
+    return (group: FormGroup) => {
+      const password = group.controls[passwordKey];
+      const confirmPassword = group.controls[confirmPasswordKey];
+
+      if (password.value === confirmPassword.value) {
+        return;
+      } else {
+        confirmPassword.setErrors({
+          notEqualToPassword: true
+        });
+      }
+    };
   }
 
   ngOnInit(): void {
   }
 
   onSubmit(signupform) {
-    console.log(signupform.value);
+    const email: string = signupform.value.email;
+    const password: string = signupform.value.password;
+    const  firstName: string = signupform.value.firstName;
+    const  lastName: string = signupform.value.lastName;
+
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+      .then((response) => {
+        // console.log(response);
+
+        const randomNumber = Math.floor(Math.random() * 1000);
+
+        response.user.updateProfile({
+          displayName: firstName + ' ' + lastName,
+          photoURL: 'https://api.adorable.io/avatars/' + randomNumber
+        }).then(() => {
+            this.message = 'You have been signed up sucessfully. Please Login.';
+            console.log(this.message);
+        });
+      }).catch((error) => {
+        console.log(error);
+        this.userError = error;
+    });
   }
 }
